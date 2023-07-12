8cdab75dd0b7fb07b539ba03d9105da14b0a4621
liumeng <rrsyycm@gmail.com>
2023-07-12 21:09:14 +0800

    修改部分接口

--- src/mock/v1/modules/demo.ts
+++ src/mock/v1/modules/demo.ts
@@ -14,15 +14,15 @@ export const indexInfo = defineMock({
                     'https://picsum.photos/720/300',
                     'https://picsum.photos/720/300',
                     'https://picsum.photos/720/300',
-                    'https://picsum.photos/720/300',
+                    'https://picsum.photos/720/300'
                 ],
                 grid: [
                     {
                         url: 'page/index/index',
                         image: 'https://picsum.photos/80/80',
                         'title|+1': 1,
-                        'class|+1': 1,
-                    },
+                        'class|+1': 1
+                    }
                 ],
                 notice: ['一一二二三三四五', '七七七八八九九'],
                 list: [
@@ -30,13 +30,13 @@ export const indexInfo = defineMock({
                         rightDetail: {
                             title: '标题',
                             subtitle: '副标题',
-                            time: '有效期时间文本',
+                            time: '有效期时间文本'
                         },
                         type: '小说',
-                        thumb: 'https://picsum.photos/100/100',
-                    },
-                ],
-            },
+                        thumb: 'https://picsum.photos/100/100'
+                    }
+                ]
+            }
         });
     },
     '/api/v1/index/shop': () => {
@@ -47,15 +47,15 @@ export const indexInfo = defineMock({
                     'https://picsum.photos/720/300',
                     'https://picsum.photos/720/300',
                     'https://picsum.photos/720/300',
-                    'https://picsum.photos/720/300',
+                    'https://picsum.photos/720/300'
                 ],
                 grid: [
                     {
                         url: 'page/index/index',
                         image: 'https://picsum.photos/80/80',
                         'title|+1': 1,
-                        'class|+1': 1,
-                    },
+                        'class|+1': 1
+                    }
                 ],
                 notice: ['一一二二三三四五', '七七七八八九九'],
                 list: [
@@ -63,13 +63,13 @@ export const indexInfo = defineMock({
                         rightDetail: {
                             title: '标题',
                             subtitle: '副标题',
-                            time: '有效期时间文本',
+                            time: '有效期时间文本'
                         },
                         type: '小说',
-                        thumb: 'https://picsum.photos/100/100',
-                    },
-                ],
-            },
+                        thumb: 'https://picsum.photos/100/100'
+                    }
+                ]
+            }
         });
     },
     '[POST]/api/v1/user/login': () => {
@@ -84,57 +84,57 @@ export const indexInfo = defineMock({
                     auth: {
                         card: {
                             name: '刘老六',
-                            cardId: '152104199312251919',
-                        },
-                    },
+                            cardId: '152104199312251919'
+                        }
+                    }
                 },
                 token: '185|z8zw9AdGA0Gnxv5E92PjWw3jNNWFTfQVm6wn1Yrv',
-                refreshToken: '185|z8zw9AdGA0Gnxv5E92PjWw3jNNWFTfQVm6wn1Yrv',
-            },
+                refreshToken: '185|z8zw9AdGA0Gnxv5E92PjWw3jNNWFTfQVm6wn1Yrv'
+            }
         });
     },
     '[POST]/api/v1/phone/code/send': () => {
         return createMock({
             data: {},
             message: '发送成功',
-            code: ResultEnum.SUCCESS,
+            code: ResultEnum.SUCCESS
         });
     },
     '[POST]/api/v1/image/upload': (request) => {
         return createMock({
             data: {
-                url: request.data.url,
+                url: request.data.url
             },
             message: '设置成功',
-            code: ResultEnum.SUCCESS,
+            code: ResultEnum.SUCCESS
         });
     },
     '[POST]/api/v1/user/set/nickname': (request) => {
         return createMock({
             data: {
-                userName: request.data.userName,
+                userName: request.data.userName
             },
             message: '设置成功',
-            code: ResultEnum.SUCCESS,
+            code: ResultEnum.SUCCESS
         });
     },
     '[POST]/api/v1/phone/code/verify': (request) => {
         return createMock({
             data: {
-                phone: request.data.phone,
+                phone: request.data.phone
             },
             message: (request.data.code as number) !== 404 ? '修改成功' : '修改失败',
-            code: (request.data.code as number) !== 404 ? ResultEnum.SUCCESS : ResultEnum.ERROR,
+            code: (request.data.code as number) !== 404 ? ResultEnum.SUCCESS : ResultEnum.ERROR
         });
     },
     '[POST]/api/v1/user/card/verify': (request) => {
         return createMock({
             data: {
                 name: request.data.name,
-                cardId: request.data.cardId,
+                cardId: request.data.cardId
             },
             message: '修改成功',
-            code: ResultEnum.SUCCESS,
+            code: ResultEnum.SUCCESS
         });
     },
     '/api/v1/user/bespeak': () => {
@@ -152,15 +152,15 @@ export const indexInfo = defineMock({
                             '14:00-15:00',
                             '15:00-16:00',
                             '16:00-17:00',
-                            '17:00-18:00',
+                            '17:00-18:00'
                         ],
                         'label|0-100': 1,
-                        'status|1': [true, false],
-                    },
+                        'status|1': [true, false]
+                    }
                 ],
                 message: '修改成功',
-                code: ResultEnum.SUCCESS,
+                code: ResultEnum.SUCCESS
             })
         );
-    },
+    }
 });
